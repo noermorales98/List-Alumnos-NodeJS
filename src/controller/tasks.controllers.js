93806722515e512ca5521a8b1a643c4d3dedbbd4
noeli <noe.rmorales98@gmail.com>
2022-03-16 07:32:34 -0500

    feat tast.controller.js

--- src/controller/tasks.controllers.js
+++ src/controller/tasks.controllers.js
@@ -1,17 +1,53 @@
 //definimos conmo nos movemos y que parametros pasamos
 import miDB from "../models/miDB";
 
-export const rendermiDB = async(req, res) => {
-    try{
-        //aqui vamos a buscar las tareas
-        const tasks = await miDB.find().lean();
-        //renderizar la vista
-        res.render("index",{
-            tasks,
-        }),
-    }
-    catch(error){
-        console.log({error});
-        return res.render("error",{errorMessenge: error.messege});
-    }
+export const renderTasks = async (req, res) => {
+  try {
+    //aqui vamos a buscar las tareas
+    const tasks = await miDB.find().lean();
+    //renderizar la vista
+    res.render("index", {
+      tasks,
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.render("error", { errorMessage: error.message });
+  }
+};
+
+
+export const createTask = async (req, res, next) => {
+  try {
+    const task = new miDB(req.body);
+    await task.save();
+    res.redirect("/");
+  } catch (error) {
+    return res.render("error", { errorMessage: error.message });
+  }
+};
+
+//para listar las tareas
+export const taskToggleDone = async (req, res, next) => {
+  let { id } = req.params;
+  const task = await miDB.findById(id);
+  task.done = !task.done;
+  await task.save();
+  res.redirect("/");
+};
+
+export const renderTaskEdit = async (req, res, next) => {
+  const task = await miDB.findById(req.params.id).lean();
+  res.render("edit", { task });
+};
+
+export const editTask = async (req, res, next) => {
+  const { id } = req.params;
+  await miDB.updateOne({ _id: id }, req.body);
+  res.redirect("/");
+};
+
+export const deleteTask = async (req, res, next) => {
+  let { id } = req.params;
+  await miDB.remove({ _id: id });
+  res.redirect("/");
 };
